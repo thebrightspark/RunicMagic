f7b7a2f98cefeac36b6c2dca8ffe91b2481db0a0
mpassey <mpassey@amelco.co.uk>
2018-11-20 18:06:51 +0000

    Implementing magic experience/levels

---
+++ src/main/java/brightspark/runicmagic/capability/CapLevel.java
@@ -0,0 +1,132 @@
+package brightspark.runicmagic.capability;
+
+import brightspark.runicmagic.LevelManager;
+import brightspark.runicmagic.RunicMagic;
+import brightspark.runicmagic.init.RMCapabilities;
+import brightspark.runicmagic.util.RunicMagicException;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
+
+// https://runescape.fandom.com/wiki/Experience
+public interface CapLevel extends RMCapability
+{
+	ResourceLocation RL = new ResourceLocation(RunicMagic.MOD_ID, "level");
+
+	static RMCapabilityProvider<CapLevel> getProvider()
+	{
+		return new RMCapabilityProvider<>(RMCapabilities.LEVEL);
+	}
+
+	int getLevel();
+
+	void setLevel(EntityPlayerMP player, int level);
+
+	int getExperience();
+
+	void setExperience(EntityPlayerMP player, int experience);
+
+	int addExperience(EntityPlayerMP player, int amount);
+
+	class Impl implements CapLevel
+	{
+		private int level, experience, levelMaxExp;
+
+		@Override
+		public int getLevel()
+		{
+			return level;
+		}
+
+		@Override
+		public void setLevel(EntityPlayerMP player, int level)
+		{
+			boolean changed = this.level != level;
+			if(changed)
+			{
+				this.level = level;
+				calcLevel(true);
+				dataChanged(player);
+			}
+		}
+
+		@Override
+		public int getExperience()
+		{
+			return experience;
+		}
+
+		@Override
+		public void setExperience(EntityPlayerMP player, int experience)
+		{
+			boolean changed = this.experience != experience;
+			if(changed)
+			{
+				this.experience = experience;
+				calcLevel(true);
+				dataChanged(player);
+			}
+		}
+
+		@Override
+		public int addExperience(EntityPlayerMP player, int amount)
+		{
+			experience += amount;
+			calcLevel(false);
+			dataChanged(player);
+			return experience;
+		}
+
+		private void calcLevel(boolean force)
+		{
+			if(level > 0 && levelMaxExp > 0 && experience < levelMaxExp)
+				return;
+			if(force)
+			{
+				level = LevelManager.getLevelForXp(experience);
+				levelMaxExp = LevelManager.get(level);
+			}
+			else
+			{
+				if(level <= 0)
+				{
+					level = LevelManager.getLevelForXp(experience);
+					if(level <= 0)
+						throw new RunicMagicException("Can't determine level for xp: " + experience);
+				}
+				if(levelMaxExp <= 0)
+					levelMaxExp = LevelManager.get(level);
+				while(experience >= levelMaxExp)
+				{
+					level++;
+					levelMaxExp = LevelManager.get(level);
+				}
+			}
+		}
+
+		@Override
+		public void dataChanged(EntityPlayerMP player)
+		{
+			//TODO: Send data sync packet to client player
+		}
+
+		@Override
+		public NBTTagCompound serializeNBT()
+		{
+			NBTTagCompound nbt = new NBTTagCompound();
+			nbt.setInteger("xp", experience);
+			return nbt;
+		}
+
+		@Override
+		public void deserializeNBT(NBTTagCompound nbt)
+		{
+			experience = nbt.getInteger("xp");
+			//Calculate new level as necessary
+			if(level <= 0 || levelMaxExp <= 0)
+				calcLevel(true);
+			else if(experience >= levelMaxExp)
+				calcLevel(false);
+		}
+	}
+}
--- src/main/java/brightspark/runicmagic/init/RMCapabilities.java
+++ src/main/java/brightspark/runicmagic/init/RMCapabilities.java
@@ -1,5 +1,6 @@
 package brightspark.runicmagic.init;
 
+import brightspark.runicmagic.capability.CapLevel;
 import brightspark.runicmagic.capability.RMCapabilityStorage;
 import brightspark.runicmagic.capability.CapSpell;
 import net.minecraft.entity.Entity;
@@ -11,10 +12,13 @@ public class RMCapabilities
 {
 	@CapabilityInject(CapSpell.class)
 	public static final Capability<CapSpell> SPELL = null;
+	@CapabilityInject(CapLevel.class)
+	public static final Capability<CapLevel> LEVEL = null;
 
 	public static void init()
 	{
 		CapabilityManager.INSTANCE.register(CapSpell.class, new RMCapabilityStorage<>(), CapSpell.Impl::new);
+		CapabilityManager.INSTANCE.register(CapLevel.class, new RMCapabilityStorage<>(), CapLevel.Impl::new);
 	}
 
 	public static boolean hasSpells(Entity player)
@@ -25,5 +29,15 @@ public class RMCapabilities
 	public static CapSpell getSpells(Entity player)
 	{
 		return player.getCapability(SPELL, null);
+	}
+
+	public static boolean hasLevel(Entity player)
+	{
+		return player.hasCapability(LEVEL, null);
+	}
+
+	public static CapLevel getLevel(Entity player)
+	{
+		return player.getCapability(LEVEL, null);
 	}
 }
