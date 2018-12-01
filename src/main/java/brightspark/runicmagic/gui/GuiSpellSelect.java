9bfdcae3eec21c97d882967421519317ac927cc1
bright_spark <markpassey@btinternet.com>
2018-12-01 04:48:50 +0000

    Number of runes in inventory are now cached in a map Spell buttons are now enabled/disabled correctly Improved Gui tooltips

--- src/main/java/brightspark/runicmagic/gui/GuiSpellSelect.java
+++ src/main/java/brightspark/runicmagic/gui/GuiSpellSelect.java
@@ -25,7 +25,6 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class GuiSpellSelect extends RMGuiScreen
 {
@@ -34,6 +33,8 @@ public class GuiSpellSelect extends RMGuiScreen
     private static final int buttonStartPos = 12;
     private static final int buttonGridSize = 8;
     private static final String TAB = "  ";
+    private final Map<RuneType, Integer> runesInInv = new HashMap<>();
+    private final int playerLevel;
     private EntityPlayer player;
     private CapSpell capSpell;
 
@@ -42,27 +43,69 @@ public class GuiSpellSelect extends RMGuiScreen
         super("spell_select", 196, 196);
         this.player = player;
         capSpell = RMCapabilities.getSpells(player);
+        playerLevel = RMCapabilities.getLevel(player).getLevel();
     }
 
     public void onSpellCapChange()
     {
         RunicMagic.LOG.info("Spell cap changed!");
+        updateCache();
+        updateButtons();
+    }
+
+    private void updateCache()
+    {
+        //Update cached runes in inv
+        runesInInv.clear();
+        player.inventory.mainInventory.forEach(stack -> {
+            if(stack.getItem() == RMItems.rune)
+            {
+                RuneType type = ((ItemRuneTypeBase) stack.getItem()).getRuneType(stack.getMetadata());
+                if(type != null)
+                {
+                    int count = stack.getCount();
+                    runesInInv.compute(type, (runeType, num) -> num == null ? count : num + count);
+                }
+            }
+        });
+        Pair<ItemStack, EnumHand> held = CommonUtils.findHeldStaff(player);
+        RuneType runeType = held != null ? ItemStaff.getRuneType(held.getKey()) : null;
+        if(runeType != null)
+            runesInInv.put(runeType, -1);
+    }
+
+    private void updateButtons()
+    {
+        //Update buttons
         Spell selectedSpell = capSpell.getSelectedSpell();
         Map<Spell, Long> cooldowns = capSpell.getCooldowns(mc.world);
         buttonList.forEach(button -> {
             if(button instanceof ButtonSpell)
             {
                 ButtonSpell buttonSpell = (ButtonSpell) button;
+                buttonSpell.enabled = playerLevel >= buttonSpell.spell.getLevel() && hasRunesInInv(buttonSpell.spell.getCost());
                 buttonSpell.selected = buttonSpell.spell == selectedSpell;
                 buttonSpell.cooldown = cooldowns.getOrDefault(buttonSpell.spell, 0L);
             }
         });
     }
 
+    private boolean hasRunesInInv(Map<RuneType, Short> cost)
+    {
+        Map<RuneType, Short> costCopy = new HashMap<>(cost);
+        //Remove from the costCopy all runes in the runesInInv
+        runesInInv.forEach((runeType, num) ->
+                costCopy.computeIfPresent(runeType, (costType, costNum) ->
+                        num < 0 ? null : num >= costNum ? null : (short) (costNum - num)));
+        //If costCopy is empty, then we had enough runes
+        return costCopy.isEmpty();
+    }
+
     @Override
     public void initGui()
     {
         super.initGui();
+        updateCache();
         //Add buttons
         List<Spell> spells = RMSpells.getSortedSpells();
         Map<Spell, Long> cooldowns = capSpell.getCooldowns(mc.world);
@@ -73,30 +116,9 @@ public class GuiSpellSelect extends RMGuiScreen
             int buttonY = buttonStartPos + (i / buttonGridSize * buttonSpacing);
             Spell spell = spells.get(i);
             ButtonSpell button = new ButtonSpell(i, buttonX, buttonY, spell, cooldowns.getOrDefault(spell, 0L), spell == selectedSpell);
+            button.enabled = playerLevel >= spell.getLevel() && hasRunesInInv(spell.getCost());
             addButton(button);
         }
-    }
-
-    private Map<RuneType, Integer> getNumInInventory(Set<RuneType> runeTypes)
-    {
-        Map<RuneType, Integer> counts = new HashMap<>(runeTypes.size());
-        for(int i = 0; i < player.inventory.getSizeInventory(); i++)
-        {
-            ItemStack stack = player.inventory.getStackInSlot(i);
-            if(stack.getItem() == RMItems.rune)
-            {
-                RuneType type = ((ItemRuneTypeBase) stack.getItem()).getRuneType(stack.getMetadata());
-                if(type == null || !runeTypes.contains(type))
-                    continue;
-                int count = stack.getCount();
-                counts.compute(type, (runeType, num) -> num == null ? count : num + count);
-            }
-        }
-        Pair<ItemStack, EnumHand> held = CommonUtils.findHeldStaff(player);
-        RuneType runeType = ItemStaff.getRuneType(held.getKey());
-        if(runeType != null)
-            counts.put(runeType, -1);
-        return counts;
     }
 
     @Override
@@ -106,7 +128,9 @@ public class GuiSpellSelect extends RMGuiScreen
         if(buttonSpell != null)
         {
             Spell spell = buttonSpell.spell;
-            tooltip.add(spell.getSpellType().getMagicType().getTextColour() + I18n.format(spell.getUnlocName()));
+            int spellLevel = spell.getLevel();
+            TextFormatting levelColour = playerLevel >= spellLevel ? TextFormatting.LIGHT_PURPLE : TextFormatting.DARK_PURPLE;
+            tooltip.add(levelColour + "[" + spell.getLevel() + "] " + spell.getSpellType().getMagicType().getTextColour() + I18n.format(spell.getUnlocName()));
             tooltip.add(TextFormatting.GRAY + spell.getSpellType().getTranslation());
             if(!spell.isSelectable())
                 tooltip.add("Will cast on click");
@@ -119,9 +143,8 @@ public class GuiSpellSelect extends RMGuiScreen
                 tooltip.add(TAB + "None");
             else
             {
-                Map<RuneType, Integer> numInInv = getNumInInventory(cost.keySet());
                 cost.forEach(((runeType, num) -> {
-                    int typeInInv = numInInv.getOrDefault(runeType, 0);
+                    int typeInInv = runesInInv.getOrDefault(runeType, 0);
                     String typeInInvText = typeInInv < 0 ? "∞" : Integer.toString(typeInInv);
                     TextFormatting colour = typeInInv < 0 || typeInInv >= num ? TextFormatting.GREEN : TextFormatting.RED;
                     tooltip.add(colour + String.format("%s/%s %s", typeInInvText, num, runeType.getTranslation()));
