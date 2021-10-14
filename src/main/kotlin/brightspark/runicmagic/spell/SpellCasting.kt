49c994db1a5df15725502aab369bc0df3a3e6726
bright_spark <markpassey@btinternet.com>
2021-10-14 21:22:13 +0100

    Fixed client side spell casting too long

--- src/main/kotlin/brightspark/runicmagic/spell/SpellCasting.kt
+++ src/main/kotlin/brightspark/runicmagic/spell/SpellCasting.kt
@@ -6,6 +6,7 @@ import brightspark.runicmagic.init.RMSpells
 import brightspark.runicmagic.message.RemoveSpellCastingMessage
 import brightspark.runicmagic.model.RuneType
 import brightspark.runicmagic.model.SpellCastData
+import brightspark.runicmagic.util.onClient
 import brightspark.runicmagic.util.onServer
 import brightspark.runicmagic.util.sendToAll
 import net.minecraft.entity.player.PlayerEntity
@@ -34,6 +35,14 @@ class SpellCasting : INBTSerializable<CompoundNBT> {
 
 	fun update(world: World, player: PlayerEntity, isClientSideSinglePlayer: Boolean): Boolean {
 		var shouldCancel = spell.updateCasting(world, player, progress)
+
+		// Client side might update the spell casting longer than the cast time while we wait for the server to execute
+		// the spell. So we make sure to finish it before the progress runs past the cast time.
+		world.onClient {
+			if (progress >= spell.castTime)
+				return true
+		}
+
 		// If on a single player world, the handler class is shared between client and server,
 		// so we only want to update progress once per tick
 		if (!isClientSideSinglePlayer)
